b69c36f8278a5515f29b71360cd810fda26c3d98
roooood <30avash.une30@gmail.com>
2019-10-15 13:34:04 +0330

    ok

--- config/settings.js
+++ config/settings.js
@@ -4,15 +4,11 @@ var settings = {
   path: path.normalize(path.join(__dirname, '..')),
   port: process.env.NODE_PORT || 2657,
   privateKey: 'my private key',
-  google: {
-    ref: 'https://www.google.com/recaptcha/api/siteverify?',
-    secret: '6LdIWasUAAAAAD5NV6JP4LQX-n3de2gNvsKa0ZPF'
-  },
   database: {
     protocol: "mysql", // or "mysql"
     query: { pool: true },
     host: "localhost",
-    database: "bitbaz",
+    database: "trade",
     user: "root",
     password: ""
   }
